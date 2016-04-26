e626699a97ebecd71ae6550b649f18978c817ae7
MrRoboman <rkayson@gmail.com>
2016-04-26 15:27:49 -0700

    backend user auth working

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,3 +1,10 @@
 class Api::SessionsController < ApplicationController
+  def create
+    render json: {Youre: "online!"}
+  end
 
+  def destroy
+    render json: {Youre: "offline!"}
+
+  end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,23 @@
 class Api::UsersController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      login!(user)
+      render :show
+    else
+      # error
+    end
+  end
+
   def show
     json = {}
     json[:username] = current_user.username if current_user
     render json: json
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
