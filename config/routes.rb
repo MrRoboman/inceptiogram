bf346d8864edcfe4565d8c3adffdef1c3c6483ae
MrRoboman <rkayson@gmail.com>
2016-04-26 14:42:56 -0700

    auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
 
 	root to: 'static_pages#root.html'
 
+	namespace :api, default: { format: :json } do
+		resource :user, only: [:create]
+		resource :session, only: [:create, :destroy]
+	end
+
 end
