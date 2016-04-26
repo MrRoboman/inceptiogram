5127988325a4eb1764f8f72330f276a848fa0d7a
MrRoboman <rkayson@gmail.com>
2016-04-26 13:42:55 -0700

    changed hello world to inceptiogram

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,13 +1,12 @@
 var React = require('react');
- 
+
  module.exports = React.createClass({
  	render: function () {
  		return(
  			<div>
- 				Hello World!
+ 				Inceptiogram
  				{this.props.children}
  			</div>
  		);
  	}
  });
-
