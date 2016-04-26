1ab0af0e9348770537890b7d341e887214075c12
MrRoboman <rkayson@gmail.com>
2016-04-26 13:57:14 -0700

    updated application.rb at directions suggestion

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module Inceptiogram
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # added by Rob!
+    # Heroku subheading
+    # https://github.com/appacademy/capstone-project-curriculum/blob/master/readings/asset-pipeline.md
+    config.assets.initialize_on_precompile = false
   end
 end
