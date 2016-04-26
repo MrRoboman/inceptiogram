915b129175b819a50fe6d4fc5da87b2fb217e917
MrRoboman <rkayson@gmail.com>
2016-04-26 14:44:31 -0700

    generated users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 	root to: 'static_pages#root.html'
 
 	namespace :api, default: { format: :json } do
-		resource :user, only: [:create]
+		resource :user, only: [:create, :show]
 		resource :session, only: [:create, :destroy]
 	end
 
