156b7ddbf8ac7e5369be67df4c9cceee6a472ed4
MrRoboman <rkayson@gmail.com>
2016-04-28 12:17:34 -0700

    cleaned up querying on backend

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -3,7 +3,8 @@ class Api::CommentsController < ApplicationController
     comment = Comment.new(comment_params)
     comment.user_id = current_user.id
     if comment.save
-      @picture = comment.picture
+      @picture = Picture.includes(:owner, comments: [:author])
+                        .find_by_id(comment.picture_id)
       render :create
     else
       debugger
