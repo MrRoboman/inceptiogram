17283a953809f6502a8514cea21ce51de7feb4dd
MrRoboman <rkayson@gmail.com>
2016-04-28 09:13:08 -0700

    migrated comments

--- db/migrate/20160428160957_create_comments.rb
+++ db/migrate/20160428160957_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.string :body, null: false
+      t.integer :user_id, index: true, null: false;
+      t.integer :picture_id, index: true, null: false;
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428143712) do
+ActiveRecord::Schema.define(version: 20160428160957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "body",       null: false
+    t.integer  "user_id",    null: false
+    t.integer  "picture_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["picture_id"], name: "index_comments_on_picture_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "pictures", force: :cascade do |t|
     t.string   "url",        null: false
