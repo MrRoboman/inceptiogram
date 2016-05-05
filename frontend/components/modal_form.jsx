4b3ce84520708a4dca0685db6538ea3cc6977baa
MrRoboman <rkayson@gmail.com>
2016-05-05 13:57:44 -0700

    modal form heart updates color when clicked

--- frontend/components/modal_form.jsx
+++ frontend/components/modal_form.jsx
@@ -1,14 +1,27 @@
 var React = require('react');
 var ClientActions = require('../actions/client_actions');
 var LikeButton = require('./like_button');
+var PictureStore = require('../stores/picture_store');
 
 var ModalForm = React.createClass({
   getInitialState: function() {
-    return {formText: ""};
+    return {formText: "", picture: this.props.picture};
   },
 
   onFormTextChange: function(e) {
     this.setState({formText: e.target.value});
+  },
+
+  componentDidMount: function() {
+    this.listener = PictureStore.addListener(this.onChange);
+  },
+
+  componentWillUnmount: function() {
+    this.listener.remove();
+  },
+
+  onChange: function() {
+    this.setState({picture: PictureStore.getSinglePicture()});
   },
 
   onSubmit: function(e) {
@@ -21,7 +34,7 @@ var ModalForm = React.createClass({
   render: function() {
     return (
       <div className="modal-form">
-        <LikeButton picture={this.props.picture}/>
+        <LikeButton picture={this.state.picture}/>
         <form onSubmit={this.onSubmit}>
           <input className="comment-text-field"
                  type="text"
